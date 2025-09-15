a1b34379da3baffb83e44455ddb7017ae0f69b8e
yuyuyunochan <larina.vasilina@list.ru>
2025-09-16 01:01:09 +0700

    Delete final_project directory

--- final_project/final_backend_project/Models/Dto/ArticleUpdateDto.cs
+++
@@ -1,8 +0,0 @@
-namespace final_backend_project.Models.Dto
-{
-    public class ArticleUpdateDto
-    {
-        public string? Title { get; set; }
-        public string? Content { get; set; }
-    }
-}
--- final_project/final_backend_project/Models/UpdateArticleStatusRequest.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace final_backend_project.Models
-{
-    public class UpdateArticleStatusRequest
-    {
-        public string Status { get; set; }
-    }
-}
