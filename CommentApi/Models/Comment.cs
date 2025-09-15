99937286c971e816c4293e5b9ec2f96a75b90231
yuyuyunochan <larina.vasilina@list.ru>
2025-09-16 01:00:14 +0700

    Delete CommentApi directory

--- CommentApi/Models/Comment.cs
+++
@@ -1,9 +0,0 @@
-namespace CommentApi.Models;
-
-public class Comment
-{
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public required string Email { get; set; }
-    public required string Body { get; set; }
-}
